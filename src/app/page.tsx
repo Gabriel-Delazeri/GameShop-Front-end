23bbdb6f3532a1ae19c7b7407ee5fd5697630eaa
Gabriel-Delazeri <gbldelazeri@gmail.com>
2024-09-26 20:50:07 -0300

    feat: future game responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {findAll} from "@/service/gameService";
 import {Game} from "@/types/game";
 import Link from "next/link";
 import {BadgeDollarSign} from "lucide-react";
+import "./styles.css"
 
 export default function Home() {
     const [games, setGames] = useState<Game[]>([]);
@@ -22,19 +23,19 @@ export default function Home() {
                 ></div>
                 <div className="overlay"></div>
                 <div className="home-container">
-                    <div className={`flex flex-col gap-2`}>
-                        <img
-                            src={"/game/covers/gta6.png"}
-                            className={`rounded-tl-xl rounded-br-xl h-72 object-cover`}
-                        />
-                        <div className={`flex flex-row justify-between items-end`}>
+                    <div className={`incoming-game-container`}>
+                            <img
+                                alt={`Incoming game cover`}
+                                src={"/game/covers/gta6.png"}
+                            />
+                        <div className={`info-container`}>
                             <div className="flex flex-col">
-                                <div className="text-xl font-semibold uppercase">
+                                <span className="title">
                                     Grand Theft Auto VI
-                                </div>
-                                <div className="text-xs font-semibold text-zinc-400">
+                                </span>
+                                <span className="subtitle">
                                     Coming 2026
-                                </div>
+                                </span>
                             </div>
                             <button className={`btn-primary`}>
                                 Pre-Order now
@@ -42,7 +43,8 @@ export default function Home() {
                         </div>
                     </div>
                     <div className={`flex flex-col gap-2 mt-4`}>
-                        <h1 className={`font-bold text-lg flex flex-row gap-1 items-center`}><BadgeDollarSign/> On Sale</h1>
+                        <h1 className={`font-bold text-lg flex flex-row gap-1 items-center`}><BadgeDollarSign/> On Sale
+                        </h1>
                         <div className={`grid grid-cols-2 gap-2`}>
 
                             {games.map(game => (
@@ -57,7 +59,8 @@ export default function Home() {
                                     <div>
                                         <div className={`font-semibold`}>{game.title}</div>
                                         <div className={`flex flex-row items-center gap-2`}>
-                                            <div className={`text-xs line-through text-zinc-400`}>${game.originalPrice}</div>
+                                            <div
+                                                className={`text-xs line-through text-zinc-400`}>${game.originalPrice}</div>
                                             <div>${game.currentPrice}</div>
                                         </div>
                                     </div>
